c6f1da761ab0c8fc29404e075c14f34e7d7e8ae8
Ubuntu <ubuntu@ip-172-31-17-217.ap-southeast-1.compute.internal>
2017-04-08 14:49:04 +0000

    using soft link to create a reference to storm binary folder

--- deploy_nimbus.sh
+++ deploy_nimbus.sh
@@ -16,14 +16,16 @@ echo "start to compile storm from source file..."
 cd storm
 git fetch origin
 git merge origin/intra-task-parallelism
-mvn clean install -DskipTests
+mvn install -DskipTests
 cd storm-dist/binary/
-mvn clean package
 mvn clean package -Dgpg.skip
-cp target/apache-storm-0.11.0-SNAPSHOT.tar.gz $ROOT_PATH
+cd target
+tar xvf apache-storm-0.11.0-SNAPSHOT.tar.gz
+ln -s $PWD/apache-storm-0.11.0-SNAPSHOT $ROOT_PATH/apache-storm-0.11.0-SNAPSHOT
+#cp target/apache-storm-0.11.0-SNAPSHOT.tar.gz $ROOT_PATH
 
 cd $ROOT_PATH
-echo "extracting the binary code..."
-tar xvf apache-storm-0.11.0-SNAPSHOT.tar.gz
+#echo "extracting the binary code..."
+#tar xvf apache-storm-0.11.0-SNAPSHOT.tar.gz
 
 bash generate_nimbus_config.sh
